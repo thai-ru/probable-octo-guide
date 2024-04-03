cd1f9cb2a471b65e8a6097de6fbc14b641749234
thai-ru <robinthairu34@gmail.com>
2024-04-03 11:29:47 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Robinson Thairu on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
