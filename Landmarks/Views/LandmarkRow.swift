07b0fde26938448e1109de54e53c037020d11257
thai-ru <robinthairu34@gmail.com>
2024-05-02 05:09:37 +0300

    Naming previews, Grouping Previews

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -23,6 +23,9 @@ struct LandmarkRow: View {
     }
 }
 
-#Preview {
-    LandmarkRow(landmark: landmarks[0])
+#Preview("Turtle Rock") {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
 }
