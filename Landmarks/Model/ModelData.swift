b29f0d0787047224b9b465cf97bc4ebeb778cbc0
thai-ru <robinthairu34@gmail.com>
2024-05-10 21:51:13 +0300

    project organisation, useof observation for storage and environment, managing state using binding

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 
-// Load the data as an Array
+@Observable
+class ModelData {
+    
+    // Load the data as an Array
 
-var landmarks: [Landmark]  = load("landmarkData.json")
+    var landmarks: [Landmark]  = load("landmarkData.json")
+}
+
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
